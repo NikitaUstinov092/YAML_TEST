997d29a307381303a43c802b9641026492883521
Nikita Ustinov <i@nustinov.ru>
2026-01-24 14:30:14 +0300

    Добавил канвас и Debug.Log("TEAT~!21");

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("TEAT~!21");
     }
 
     // Update is called once per frame
