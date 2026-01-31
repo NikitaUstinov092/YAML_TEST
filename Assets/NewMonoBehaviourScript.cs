99e6212f7fed3bdf7165f5d78074414281581884
Nikita Ustinov <i@nustinov.ru>
2026-01-31 16:18:35 +0300

    Удалил всё нахер

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,16 +2,5 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        var i = "";
-        Debug.Log("TEAT~!21");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
