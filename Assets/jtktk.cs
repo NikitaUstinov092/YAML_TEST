9c4e32162500ee80434a9b015b9208375816d73b
Nikita Ustinov <122388971+NikitaUstinov092@users.noreply.github.com>
2026-04-16 17:14:20 +0300

    11

--- Assets/jtktk.cs
+++ Assets/jtktk.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class 1231313 : MonoBehaviour
+public class jtktk : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
